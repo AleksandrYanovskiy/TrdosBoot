cec751fbb169865c95269f31e3ec54840484021f
AleksandrYanovskiy <41021543+AleksandrYanovskiy@users.noreply.github.com>
2018-09-04 16:55:41 +0300

    Create subdir.mk

---
+++ make/src/subdir.mk
@@ -0,0 +1,23 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/TrdosBoot.cpp 
+
+OBJS += \
+./src/TrdosBoot.o 
+
+CPP_DEPS += \
+./src/TrdosBoot.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+

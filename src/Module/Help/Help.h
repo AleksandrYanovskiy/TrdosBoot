a03d4e821deca3409a4a17d61f8eaaa7d4b3b2b5
AleksandrYanovskiy <41021543+AleksandrYanovskiy@users.noreply.github.com>
2018-09-04 16:53:36 +0300

    Create Help.h

---
+++ src/Module/Help/Help.h
@@ -0,0 +1,22 @@
+/*
+ * Help.h
+ *
+ *  Created on: Nov. 2017
+ *      Author: aleksandr.yanovskiy
+ */
+
+#ifndef MODULE_HELP_HELP_H_
+#define MODULE_HELP_HELP_H_
+
+#include <iostream>
+
+namespace graf {
+
+class Help {
+public:
+	static void show(const char* ProgName);
+};
+
+} /* namespace graf */
+
+#endif /* MODULE_HELP_HELP_H_ */ 

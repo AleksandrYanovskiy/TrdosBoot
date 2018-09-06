c63eacb5b135eb11aaeee6cb0e2c12cb33295b18
AleksandrYanovskiy <41021543+AleksandrYanovskiy@users.noreply.github.com>
2018-09-06 11:44:23 +0300

    Update TrdosBoot.cpp

--- src/TrdosBoot.cpp
+++ src/TrdosBoot.cpp
@@ -1,10 +1,10 @@
 //============================================================================
 // Name        : TrdosBoot.cpp
 // Author      : Aleksandr Yanovskiy
-// Version     : 1.00
-// Copyright   : GPLv3
-// Description : Find and rename Basic faile in TRD or SCL files to boot.B
-// 				 for autorun them in tr-dos Zx Spectrum
+// Version     : 1.10
+// License     : GPLv3
+// Description : Find and rename Basic falie in TRD or SCL files to boot.B
+// 				 for autoran them in tr-dos Zx Spectrum
 //
 // www.zx-modules.de
 //
@@ -27,28 +27,43 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
+	char* cParam;
+	char* cPath;
+
+	bool isParamErros = false;
+	bool isProcessing = false;
 
 	//check arguments
-	if (argc > 1) {
-		char* cParam = argv[1];
+	if (argc == 2) {
+		cParam  = argv[1];
 
-		//showhelp
 		if ( (strcmp(cParam, "-h") == 0) || (strcmp(cParam, "--help") == 0) ) {
-			graf::Help::show(APP_NAME);
+			graf::Help::show(APP_NAME, false);
+		}
+		else if (cParam[0] == '-') {
+			isParamErros = true;
 		}
 		else {
-			//run file processing
-			try {
-				graf::Processing::run(cParam);
-			}
-			catch (std::exception &e) {
-				std::cout << "Error: " << e.what() << std::endl;
-			}
-
+			cPath = cParam;
+			isProcessing = true;
 		}
 	}
-	else {
-		graf::Help::show(APP_NAME);
+	else
+		isParamErros = true;
+
+
+	if (isProcessing) {
+	//run file processing
+		try {
+			graf::Processing::run(cPath);
+		}
+		catch (std::exception &e) {
+			std::cout << "Error: " << e.what() << std::endl;
+		}
+	}
+	else if (isParamErros) {
+		graf::Help::show(APP_NAME, true);
+		return 1;
 	}
 
 	return 0;

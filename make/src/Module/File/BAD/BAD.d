58c0f2cb8d7c88f45e0e12dbfef4efe5dee05bab
AleksandrYanovskiy <41021543+AleksandrYanovskiy@users.noreply.github.com>
2018-09-06 11:55:30 +0300

    Create BAD.d

---
+++ make/src/Module/File/BAD/BAD.d
@@ -0,0 +1,6 @@
+src/Module/File/BAD/BAD.d: ../src/Module/File/BAD/BAD.cpp \
+ ../src/Module/File/BAD/BAD.h ../src/Module/File/BAD/../File.h
+
+../src/Module/File/BAD/BAD.h:
+
+../src/Module/File/BAD/../File.h:

db74651309603125d5ecd14b4c871e60a632cbbd
AleksandrYanovskiy <41021543+AleksandrYanovskiy@users.noreply.github.com>
2018-09-06 11:45:47 +0300

    Update Help.cpp

--- src/Module/Help/Help.cpp
+++ src/Module/Help/Help.cpp
@@ -9,13 +9,25 @@
 
 namespace graf {
 
-void Help::show(const char* ProgName) {
-	std::cout << "TrdosBoot ver. 1.00" << std::endl;
-	std::cout << "usage: " << ProgName << " filename" << std::endl;
-	std::cout << "Find first Basic file with extension B, and rename it to boot in image file TRD or SCL." << std::endl;
-	std::cout << "filename - name, or extension: src or trd" << std::endl;
-	std::cout << "if set extension, processing all files with this extension" << std::endl;
-	std::cout << "Writen by Aleksandr Yanovskiy. November 2017" << std::endl;
+void Help::show(const char* ProgName, bool error) {
+	if (!error) {
+		std::cout << "TrdosBoot ver. 1.10" << std::endl;
+		std::cout << "Find first Basic file with extension B, and rename it to boot.B in image file TRD or SCL." << std::endl;
+		std::cout << std::endl;
+	}
+	else {
+		std::cout << "Parameters error!" << std::endl;
+	}
+
+	std::cout << "Usage: " << ProgName << " [path]" << std::endl;
+
+	if (!error) {
+		std::cout << "\t-h | --help \t\tshow help information" << std::endl;
+		std::cout << "\tpath \t\t\tpath name or file name where are tr-dos image files (extension SCL or TRD is required)" << std::endl;
+		std::cout << std::endl;
+		std::cout << "Writen by Aleksandr Yanovskiy. Nov. 2017" << std::endl;
+		std::cout << "https://github.com/AleksandrYanovskiy/TrdosBoot/" << std::endl;
+	}
 }
 
 

c84174a4cbcb469a0d8c95482531dea78f88bbe3
AleksandrYanovskiy <41021543+AleksandrYanovskiy@users.noreply.github.com>
2018-09-06 11:59:56 +0300

    Delete BAD.d

--- make/src/Module/File/BAD/BAD.d
+++
@@ -1,6 +0,0 @@
-src/Module/File/BAD/BAD.d: ../src/Module/File/BAD/BAD.cpp \
- ../src/Module/File/BAD/BAD.h ../src/Module/File/BAD/../File.h
-
-../src/Module/File/BAD/BAD.h:
-
-../src/Module/File/BAD/../File.h:

02efba79998c7294d0924ef8db6c27f2826a1ff0
AleksandrYanovskiy <41021543+AleksandrYanovskiy@users.noreply.github.com>
2018-09-06 11:45:00 +0300

    Update Processing.cpp

--- src/Processing/Processing.cpp
+++ src/Processing/Processing.cpp
@@ -15,60 +15,86 @@ namespace graf {
  * filename - single file
  *
  */
-void Processing::run(std::string fileName) {
+//void Processing::run(std::string fileName) {
+void Processing::run(char* filePath) {
 
-	std::string upperFileName = upperCase(fileName);
+	std::string upperFileName;
+	std::string upperFileExt;
+	std::string dirName;
 
-	if ( (upperFileName.length() == 3) && ( (upperFileName != "TRD") && upperFileName != "SCL") ) {
-		throw std::runtime_error("File extension must be SCL or TRD !");
-	}
+
+	//std::cout << "File path: " << filePath << std::endl;
+
+
+	//get file name
+	upperFileName = upperCase(getFileName(filePath));
+
+	if ( upperFileName.empty() )
+		throw std::runtime_error("The file name must be specified!");
+
+	//std::cout << "upper file name: " << upperFileName << std::endl;
 
 	//get extension
-	std::string fileExt = getFileExt(upperFileName);
-	if ( (fileExt.empty()) || ((fileExt != "SCL") && (fileExt != "TRD")) ) {
-		throw std::runtime_error("File extension must be SCL or TRD !");
+	upperFileExt = upperCase(getFileExt(filePath));
+
+	//std::cout << "Upper file ext: " << upperFileExt << std::endl;
+
+	if ( (upperFileExt != "SCL") && (upperFileExt != "TRD") ) {
+		throw std::runtime_error("File extension must be SCL or TRD!");
 	}
 
-	std::string dirName = ".";
+	dirName = getDirName(filePath);
+	if (dirName.empty())
+		dirName = "./";
+
+	//std::cout << "Dir name: " << dirName << std::endl;
 
 	DIR* pDir;
 	struct dirent* entry;
 	std::string upperCurrFileName;
+	std::string currFileName;
 	graf::File* pFileMod;
 	unsigned int fileCountAll = 0;
 	unsigned int fileCountOk = 0;
 	unsigned int fileCountNo = 0;
-	//todo распарсить директорию, если она понадобиться
+
 	if ( (pDir = opendir(dirName.c_str())) != NULL ) {
 		logln("Processing...");
 		while ( (entry = readdir(pDir)) != NULL ) {
+
+			//std::cout << "entry->d_name=" << entry->d_name << std::endl;
+
 			 if( ((strcmp(entry->d_name, ".") == 0) || (strcmp(entry->d_name, "..") == 0)) || (entry->d_type == DT_DIR) ) {
 				 continue;
 			 }
 
 			 upperCurrFileName = upperCase(entry->d_name);
+			 currFileName = entry->d_name;
+
 			 //compare name
-			 if (compareFileName(upperFileName, upperCurrFileName)) {
+			 if ( ( (upperFileName + "." + upperFileExt) == upperCurrFileName) || ((upperFileName == "*") && (upperFileExt == getFileExt(currFileName)) ) ) {
 				 fileCountAll++;
 				 log("\trename > ");
-				 logt(entry->d_name);
+				 logt(entry->d_name, 0, 20);
 
-				 if (fileExt == "SCL")
-					 pFileMod = new graf::SCL(entry->d_name);
-				 else /*if (fileExt == "TRD")*/
-					 pFileMod = new graf::TRD(entry->d_name);
+				 if (upperFileExt == "SCL")
+					 pFileMod = new graf::SCL(dirName + entry->d_name);
+				 else if (upperFileExt == "TRD")
+					 pFileMod = new graf::TRD(dirName + entry->d_name);
+				 else
+					 pFileMod = new graf::BAD(dirName + entry->d_name);
 
 				 try {
 					 pFileMod->rename();
 					 fileCountOk++;
-					 log("<OK>  ");
-					 log("Renamed file \"");
+					 log(" <OK>  ");
+					 logt("Renamed file \"", 2, 0);
 					 log(pFileMod->getOldName().c_str());
 					 logln("\"");
 				 }
 				 catch (std::exception &e) {
 					 fileCountNo++;
-					 log("<NO>  ");
+					 log(" <NO>  ");
 					 log("Error: ");
 					 logln(e.what());
 				 }
@@ -93,11 +119,25 @@ void Processing::run(std::string fileName) {
 }
 
 
+std::string Processing::getDirName(std::string fileName) {
+	std::string cRes = fileName.substr(0, fileName.find_last_of('/') + 1);
+	return cRes;
+}
+
 /**
  *
  */
 std::string Processing::getFileName(std::string fileName) {
-	std::string cRes = fileName.substr(0, fileName.find_last_of('.'));
+	std::size_t foundDelem = fileName.find_last_of('/') + 1;
+	std::size_t foundDote = fileName.find_last_of('.');
+
+	//std::cout << "foundDelem = " << foundDelem << std::endl;
+	//std::cout << "foundDote = " << foundDote << std::endl;
+
+	std::string cRes = fileName.substr(foundDelem, foundDote - foundDelem );
+
+	//std::cout << "cRes=" << cRes << std::endl;
+
 	return cRes;
 }
 
@@ -105,7 +145,10 @@ std::string Processing::getFileName(std::string fileName) {
  *
  */
 std::string Processing::getFileExt(std::string fileName) {
-	std::string cRes = fileName.substr(fileName.find_last_of('.') + 1);
+	std::string cRes = "";
+	std::size_t found = fileName.find_last_of('.');
+	if (found != std::string::npos)
+	  cRes = fileName.substr(found + 1);
 	return cRes;
 }
 
@@ -167,4 +210,4 @@ bool Processing::compareFileName(std::string filePattern, std::string fileName) 
 	return false;
 }
 
-} /* namespace graf */
+} /* namespace graf */ 
